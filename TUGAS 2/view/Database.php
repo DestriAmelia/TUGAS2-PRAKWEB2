1a2b91043b3895b3885b507f4b7fa7595a42248f
DestriAmelia <168881987+DestriAmelia@users.noreply.github.com>
2024-09-20 23:43:09 +0700

    Update Database.php

--- TUGAS 2/view/Database.php
+++ TUGAS 2/view/Database.php
@@ -1,22 +1,23 @@
 <?php
 
-class Database { // pembuatan class Database untuk melakukan koneksi
-  // pembuatan atribut privat agar hanya dapat diakses oleh class Database
+  // membuat kelas Database untuk koneksi
+  class Database { 
+  // membuat properti 'privat' tujuannya agar hanya dapat diakses oleh class yang ada di Database
   private $db_host = "localhost";
   private $db_user = "root";  // username database
   private $db_pass = "";  // password database
   private $db_name = "tugas_2";  // nama database
   protected $conn; // untuk menyiapkan koneksi dengan database
  
-
-  public function __construct() // untuk melakukan koneksi kedatabase
+  // method untuk melakukan koneksi kedatabase dengan fungsi 'contsruct'
+  public function __construct() 
   {
-    // inisialisasi koneksi
+    
+  // inisialisasi koneksi objek intansi dari kelas Database
     $this->conn = mysqli_connect($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
     if(!$this->conn) { // untuk menampilkan pesan jika konesi tidak tejadi
       
       echo "Gagal terhubung";
     }
   }
-
 }
